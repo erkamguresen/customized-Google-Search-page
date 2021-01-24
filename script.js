3616de1f7b9372f0be1876925f5de6812131c7cf
erkam <erkamguresen@gmail.com>
2021-01-24 16:35:36 +0100

    seprating the js code

--- script.js
+++ script.js
@@ -3,3 +3,17 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+
+function searchInputValue() {
+  // Selecting the input element and get its value
+  var inputVal = document.getElementById("searchItem").value;
+
+  //generate the link
+  var linkString = "https://www.google.com/search?q=" + inputVal;
+
+  // Displaying an alert
+  alert("Redirecting to Google results...");
+
+  //redirect web page
+  location.href = linkString;
+}
